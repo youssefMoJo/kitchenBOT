db9faeb5275d90ffdb202a0267b3fa5bf3ff32ad
Mark Armanious <markarmanus@gmail.com>
2023-06-03 15:50:16 -0230

    add eslint

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { View, TextField, Text, Button } from 'react-native-ui-lib';
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
         <Text>Lets do This</Text>
         <Button label="Next" onPress={() => console.log('clicked')}></Button>
