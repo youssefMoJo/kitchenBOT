77b558dc638048c1ada13d7d8b0f5ac08c6827c1
Mark Armanious <markarmanus@gmail.com>
2023-06-03 16:06:26 -0230

    change eslint rules

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
-import { View, TextField, Text, Button } from 'react-native-ui-lib';
+import { View, Text, Button } from 'react-native-ui-lib';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
       <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
         <Text>Lets do This</Text>
-        <Button label="Next" onPress={() => console.log('clicked')}></Button>
+        <Button label="Next" onPress={() => console.log('clicked')} />
       </View>
     </View>
   );
